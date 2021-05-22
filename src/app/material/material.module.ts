68dc033004bc495197deaeb6b3624f62590aafbe
Juan Diaz <juan.diaz@doublevpartners.com>
2021-05-22 16:52:45 -0500

    add nav and product-form component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
@@ -16,6 +20,10 @@ import {MatCardModule } from '@angular/material/card';
     MatBadgeModule,
     MatIconModule,
     MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ],
   exports: [
     MatButtonModule,
@@ -23,6 +31,10 @@ import {MatCardModule } from '@angular/material/card';
     MatBadgeModule,
     MatIconModule,
     MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ]
 })
 export class MaterialModule { }

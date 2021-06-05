f009540338334463c4f9023f0a13beae03d0c2c6
Juan Diaz <juan.diaz@doublevpartners.com>
2021-06-05 11:37:48 -0500

    API consumption

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -21,7 +21,9 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      this.product = this.productsService.getProduct(id);
+      this.productsService.getProduct(id).subscribe(product => {
+        this.product = product
+      });
     });
   }
 

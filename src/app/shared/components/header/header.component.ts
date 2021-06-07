4c226c52da15617954df13c5f577694d9d4b80ed
Juan Diaz <juan.diaz@doublevpartners.com>
2021-06-07 14:12:38 -0500

    orders

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartService } from 'src/app/core/service/cart.service';
 
 @Component({
@@ -8,14 +10,13 @@ import { CartService } from 'src/app/core/service/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total: number = 0
+  total$: Observable<number>
 
   constructor(private cartService: CartService) {
 
-    this.cartService.cart$.subscribe(products => {
-      console.log(products)
-      this.total = products.length
-    })
+    this.total$ = this.cartService.cart$
+      .pipe(map(products => products.length))
+
   }
 
   ngOnInit() {
